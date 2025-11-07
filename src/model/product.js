6ec81f0403c80e98779700abbba932c1ef06bd17
Iryna-M-html <ivmohnataya@gmail.com>
2025-11-07 22:24:34 +0200

    create model product

---
+++ src/model/product.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: {
+      type: String,
+      enum: ['Books', 'Electronics', 'Clothing', 'Other'],
+      default: 'Other',
+    },
+    description: { type: String },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Product = mongoose.model('Product', productSchema);
+// если не указано required: true поле автоматически становится optional
