1d4a467efb15daaab12e53f8b431cf687a5b13c5
Iryna-M-html <ivmohnataya@gmail.com>
2025-11-19 10:18:03 +0200

    create session model

--- src/models/user.js
+++ src/models/user.js
@@ -22,15 +22,17 @@ const userSchema = new Schema(
 );
 
 userSchema.pre('save', function (next) {
-  if (!this.username) {
-    this.username = this.email;
+  if (!this.name) {
+    this.name = this.email;
   }
   next();
 });
 
 userSchema.methods.toJSON = function () {
+  // Берёт документ и превращает его в обычный объект (this.toObject()).
   const obj = this.toObject();
+  //Удаляет поле password, чтобы оно никогда не утекло на клиент.
   delete obj.password;
-  return obj;
+  return obj; //Возвращает «очищенный» объект.
 };
 export const User = model('User', userSchema);
