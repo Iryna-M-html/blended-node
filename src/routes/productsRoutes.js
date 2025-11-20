bfa65be8c6b5b4368c1d71b6cac89c922cc8535e
Iryna-M-html <ivmohnataya@gmail.com>
2025-11-20 13:52:31 +0200

    all but dont have pagination

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -6,11 +6,26 @@ import {
   deleteProduct,
   updateProduct,
 } from '../controllers/productsController.js';
+
+import {
+  productIdSchema,
+  createProductSchema,
+  updateProductSchema,
+} from '../validations/productsValidation.js';
+
+import { celebrate } from 'celebrate';
+import { authenticate } from '../middleware/authenticate.js';
+
 const router = Router();
+router.use('/products', authenticate);
 router.get('/products', getAllProducts);
-router.get('/products/productId', getProductById);
-router.post('/products', createProduct);
-router.patch('/products/productId', updateProduct);
-router.delete('/products/productId', deleteProduct);
+router.get('/products/productId', celebrate(productIdSchema), getProductById);
+router.post('/products', celebrate(createProductSchema), createProduct);
+router.patch(
+  '/products/productId',
+  celebrate(updateProductSchema),
+  updateProduct,
+);
+router.delete('/products/productId', celebrate(productIdSchema), deleteProduct);
 
 export default router;
--- src/validations/productsValidation.js
+++ src/validations/productsValidation.js
@@ -1,5 +1,32 @@
-// import { Joi, Segments } from 'celebrate';
-// import { isValidObjectId } from 'mongoose';
-// const objectIdValidator = (value, helpers) => {
-//   return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
-// };
+import { Joi, Segments } from 'celebrate';
+import { isValidObjectId } from 'mongoose';
+const objectIdValidator = (value, helpers) => {
+  return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
+};
+export const productIdSchema = {
+  [Segments.PARAMS]: Joi.object({
+    productId: Joi.string().custom(objectIdValidator).required(),
+  }),
+};
+export const createProductSchema = {
+  [Segments.BODY]: Joi.object({
+    name: Joi.string().min(1).required(),
+    price: Joi.number().required(),
+    category: Joi.string()
+      .valid('Books', 'Electronics', 'Clothing', 'Other')
+      .default('Other')
+      .required(),
+  }),
+};
+
+export const updateProductSchema = {
+  ...productIdSchema,
+  [Segments.BODY]: Joi.object({
+    name: Joi.string().min(1).required(),
+    price: Joi.number().required(),
+    category: Joi.string()
+      .valid('Books', 'Electronics', 'Clothing', 'Other')
+      .default('Other')
+      .required(),
+  }).min(1),
+};
