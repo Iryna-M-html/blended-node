97dd57dd8deb039ac1fbf5b2cb24c133967c678b
Iryna-M-html <ivmohnataya@gmail.com>
2025-11-20 13:07:40 +0200

    +refresh token

--- src/services/auth.js
+++ src/services/auth.js
@@ -20,4 +20,17 @@ export const setSessionCookies = (res, session) => {
     sameSite: 'none',
     maxAge: FIFTEEN_MINUTES,
   });
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+    maxAge: THIRTY_DAYS,
+  });
+
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+    maxAge: THIRTY_DAYS,
+  });
 };
